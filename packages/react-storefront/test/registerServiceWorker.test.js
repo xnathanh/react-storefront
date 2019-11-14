100792f893b0482fe275d96752602b106e003779
Mark Brocato <mark.brocato@moovweb.com>
2019-11-14 13:40:26 +0200

    Fixed failing test

--- packages/react-storefront/test/registerServiceWorker.test.js
+++ packages/react-storefront/test/registerServiceWorker.test.js
@@ -4,14 +4,14 @@ describe('registerServiceWorker', () => {
   describe('getServiceWorkerURL', () => {
     it('should contain the mode id and use the public_url', () => {
       process.env.PUBLIC_URL = 'https://www.site.com'
-      window.moov = {
-        state: {
-          mode: {
-            id: 'a',
-            name: 'Mode A'
-          }
+
+      window.initialState = {
+        mode: {
+          id: 'a',
+          name: 'Mode A'
         }
       }
+
       expect(getServiceWorkerURL()).toEqual(
         'https://www.site.com/service-worker.js?moov_fetch_from=a'
       )
