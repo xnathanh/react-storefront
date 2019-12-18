1913e401e8d248d7574cb0c1c43a7b0cd08136ec
Richard van der Dys <richard.vanderdys@gmail.com>
2019-12-18 15:31:48 -0600

    Added changelog entry

--- packages/react-storefront/src/router/Router.js
+++ packages/react-storefront/src/router/Router.js
@@ -609,7 +609,6 @@ export default class Router extends EventEmitter {
     const request = { path: pathname, search, query: qs.parse(search), method: 'GET' }
     const context = new ClientContext(request)
 
-    // this.runAll(request, context, { initialLoad: true }, window.initialState)
     this.emit('after', { request, response: context, initialLoad: true })
 
     return this
